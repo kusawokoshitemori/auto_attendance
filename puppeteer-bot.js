6a1ff0b9017dd6abcbc0048f02d9771a12f7df76
草をこして森 <kyou0317kyou0317@gmail.com>
2024-10-15 15:42:18 +0900

    マイクロソフトのサインインを要求されたのでできなかった

--- puppeteer-bot.js
+++ puppeteer-bot.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const puppeteer = require("puppeteer");
 const cron = require("node-cron");
 const readline = require("readline");
@@ -8,38 +9,50 @@ const rl = readline.createInterface({
 });
 
 (async () => {
-  // 毎日15:40に実行する設定
-  cron.schedule("08 16 * * *", async () => {
+  // 毎日決まった時間に実行する設定
+  cron.schedule("42 15 * * *", async () => {
     console.log("スクリプト実行中...");
 
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
 
-    // 出席画面のサイトにアクセスし、完全に読み込まれるまで待つ
-    await page.goto("https://btnmaker.me/", {
+    // 出席画面のサイトにアクセスし、完全に読み込まれるまで待つ(個人情報漏洩の為URLをenvに)
+    await page.goto(process.env.TARGET_URL, {
       waitUntil: "load", // ページが完全に読み込まれるのを待つ
     });
 
-    // ボタンが表示されるのを待つ
-    await page.waitForSelector("div.tweetButton button.c-btn.floating", {
-      visible: true,
-    });
+    // 特定のリンクをクリック
+    await page.waitForSelector(`a[href="${process.env.CLICK_URL}"]`); // バックティックを使用
+    await Promise.all([
+      page.click(`a[href="${process.env.CLICK_URL}"]`),
+      page.waitForNavigation({ waitUntil: "networkidle2" }), // ページの遷移を待機
+    ]);
 
-    // ボタンをクリック
-    await page.waitForSelector("div.tweetButton button.c-btn.floating", {
-      visible: true,
-    });
-    await page.evaluate(() => {
-      const button = document.querySelector(
-        "div.tweetButton button.c-btn.floating"
-      );
-      if (button) {
-        button.scrollIntoView(); // ボタンをスクロールして表示させる
-        button.click(); // ボタンをクリック
-      }
-    });
+    // 五秒待つ
+    await page.waitForTimeout(5000);
+    // キーボードでメールアドレスを入力
+    await page.keyboard.type("me-ruadoresu");
 
-    console.log("ボタンが押されました！");
+    // // ボタンが表示されるのを待つ
+    // await page.waitForSelector("div.tweetButton button.c-btn.floating", {
+    //   visible: true,
+    // });
+
+    // // ボタンをクリック
+    // await page.waitForSelector("div.tweetButton button.c-btn.floating", {
+    //   visible: true,
+    // });
+    // await page.evaluate(() => {
+    //   const button = document.querySelector(
+    //     "div.tweetButton button.c-btn.floating"
+    //   );
+    //   if (button) {
+    //     button.scrollIntoView(); // ボタンをスクロールして表示させる
+    //     button.click(); // ボタンをクリック
+    //   }
+    // });
+
+    // console.log("ボタンが押されました！");
 
     // ユーザーがEnterキーを押すまで待つ
     rl.question("Enterキーを押すとブラウザが閉じます...\n", async () => {
